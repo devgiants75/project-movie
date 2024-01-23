f8aed1ebc6571097c84f30ee1914dd4d4d86e1c9
ksd3940 <ksd3940@naver.com>
2024-01-23 14:17:53 +0900

    2024/01/23 폴더작업& 영화이미지

--- moview/src/pages/index.tsx
+++ moview/src/pages/index.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 //! 애플리케이션의 각 페이지를 나타내는 컴포넌트들을 보관
 // ex) HomePage.tsx, MyPage.tsx, NoticeBoard.tsx
-export default function index() {
+export default function Index() {
   return (
-    <div>index</div>
+    <>
+    
+    </>
   )
 }
