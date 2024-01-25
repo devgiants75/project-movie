4ee3d45bf1371fbd2f397a3f3809675cd5efa271
ksd3940 <ksd3940@naver.com>
2024-01-25 16:43:32 +0900

    src 작업

---
+++ MOVIEW/src/pages/movieDetail/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div>영화상세페이지</div>
+  )
+}
---
+++ MOVIEW/src/pages/searchData/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div>통합검색결과</div>
+  )
+}
